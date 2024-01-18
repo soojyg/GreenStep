39cb62d7b806fc6ec7d40cc701522c4f6c3563b4
xueting03 <133341971+xueting03@users.noreply.github.com>
2024-01-18 22:09:32 +0800

    add comments in NewsAdminView.java

--- app/src/main/java/com/example/greenstep/NewsAdminView.java
+++ app/src/main/java/com/example/greenstep/NewsAdminView.java
@@ -27,9 +27,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import org.checkerframework.checker.nullness.qual.Nullable;
-
 import java.util.ArrayList;
 import java.util.List;
 
